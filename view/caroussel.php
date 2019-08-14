eded2490c47f28a5ec0aace5422018bfcb2c8746
Gabriel Queiroz <gabriel23002@gmail.com>
2019-08-14 14:32:27 -0300

    7°commit - Protótipo Caroussel

---
+++ view/caroussel.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Banner</title>
+    <link href="../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <link href="../css/sb-admin-2.min.css" rel="stylesheet">
+    <script type="text/javascript" src="../js/buscaCategoriaBanner.js"></script>
+    <style>
+      
+        .teste {
+            
+            width: 80%;
+            height: 40%;
+        }
+        .img-teste{
+            max-height: 40%;
+            max-width: 80%;
+        }
+    </style>
+</head>
+<div class="container" style="">
+    <div id="carouselExampleFade" style="" class="carousel slide carousel-fade" data-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active teste">
+                <img class="d-block w-100 img-teste" src="../documentos/Diego Maradona/Diagrama att2.png" alt="Primeiro Slide">
+            </div>
+            <div class="carousel-item teste">
+                <img class="d-block w-100 img-teste" src="../documentos/Diego Maradona/Diagrama att.png" alt="Segundo Slide">
+            </div>
+            <div class="carousel-item teste">
+                <img class="d-block w-100 img-teste" src="../documentos/Ivan/task-complete (1).png" alt="Terceiro Slide">
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Anterior</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
+            <span class="carousel-control-next-icon"  aria-hidden="true"></span>
+            <span class="sr-only">Próximo</span>
+        </a>
+    </div>
+</div>
+<?php include("footer.php"); ?>
---
+++ view/listarBanner.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Banner</title>
+    <link href="../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <link href="../css/sb-admin-2.min.css" rel="stylesheet">
+    <script type="text/javascript" src="../js/buscaCategoriaBanner.js"></script>
+</head>
+
+<?php include_once "sidebar.php"; ?>
+<?php include_once "../funcoes/conexao.php"; ?>
+
+
+<div class="col-12 text-center my-5">
+
+    <h1 style="font-weight: 330;"><i class="fa fa-paper-plane text-primary mr-3" aria-hidden="true"></i>Categorias - Banner</h1>
+
+
+    <div class="row">
+
+        <div class="col-12  ">
+
+            <form class="form-inline mb-3 ">
+                <input class="form-control ml-5" type="search" placeholder="Buscar..." id="buscanome" onkeyup="buscarCategoriaBanner(this.value)">
+            </form>
+
+            <div class="row" id="resultado">
+
+                <?php
+
+
+                $sql = "SELECT * FROM categoria_banner";
+                $consulta = mysqli_query($conn, $sql);
+
+                while ($dados = mysqli_fetch_assoc($consulta)) {
+                    ?>
+                <form action="post" class="col-5">
+                    <input type="hidden" name="banner" value="<?php echo $dados['categoria_banner']; ?>">
+                    <button type="submit" class="btn btn-primary ml-5 mb-3" formaction="caroussel.php" style="width: 100%;"><?php echo $dados['categoria_banner']; ?></button>
+                </form>
+                <?php } ?>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</div>
+
+</div>
+<?php include_once  "footer.php"; ?>
